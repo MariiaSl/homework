603928a8253f759f5d3a0d621a7de4700b9eb4bf
Mariia Slobodina <mvs030915@gmail.com>
2021-03-28 13:14:51 +0300

    js-hw9-sollution with data attribute

--- js9/js/script.js
+++ js9/js/script.js
@@ -4,19 +4,45 @@
 */
 
 "use strict";
+//var 1 - tab finds text by index
+// const tabsNonActive = Array.from(document.getElementsByClassName("tabs-title"));
+// const textNonActive = Array.from(document.getElementsByClassName("tabs-text"));
+// //console.log(textNonActive);
+// textNonActive.forEach((txt) => txt.classList.add("hide"));
 
-const tabsNonActive = Array.from(document.getElementsByClassName("tabs-title"));
-const textNonActive = Array.from(document.getElementsByClassName("tabs-text"));
-//console.log(textNonActive);
-textNonActive.forEach((txt) => txt.classList.add("hide"));
+// tabsNonActive.forEach((tab, index) => {
+//   tab.addEventListener("click", showTextHandler);
+//   function showTextHandler(event) {
+//     textNonActive.forEach((txt) => txt.classList.add("hide"));
+//     textNonActive[index].classList.remove("hide");
 
-tabsNonActive.forEach((tab, index) => {
+//     tabsNonActive.forEach((tab) => tab.classList.remove("active"));
+//     event.target.classList.add("active");
+//   }
+// });
+
+//var2 - tab finds text by data attribute
+
+let tabsNonActive = Array.from(document.getElementsByClassName("tabs-title"));
+let textNonActive = Array.from(document.getElementsByClassName("tabs-text"));
+textNonActive.forEach((txt) => {
+  txt.classList.add("hide");
+});
+
+tabsNonActive.forEach((tab) => {
   tab.addEventListener("click", showTextHandler);
-  function showTextHandler(event) {
-    textNonActive.forEach((txt) => txt.classList.add("hide"));
-    textNonActive[index].classList.remove("hide");
 
-    tabsNonActive.forEach((tab) => tab.classList.remove("active"));
-    event.target.classList.add("active");
+  function showTextHandler(event) {
+    textNonActive.forEach((txt) => {
+      if (event.target.getAttribute("id") === txt.getAttribute("data-about")) {
+        tabsNonActive.forEach((el) => {
+          el.classList.remove("active");
+          txt.classList.remove("hide");
+        });
+        event.target.classList.add("active");
+      } else {
+        txt.classList.add("hide");
+      }
+    });
   }
 });
